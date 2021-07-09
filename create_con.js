0b716f67f2ed4eda0f34d4fb7e19624475021026
nimesh <2018cs131@stu.ucsc.cmb.ac.lk>
2021-07-09 07:18:34 +0530

    w3

---
+++ create_con.js
@@ -0,0 +1,11 @@
+var mysql = require('mysql');
+ var con = mysql.createConnection({
+     host: "localhost",
+     user: "root",
+     password: ""
+ });
+
+ con.connect(function(err){
+     if(err) throw err;
+     console.log("Connected");
+ });
